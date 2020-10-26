79119a5a0d65d6b2a8f40b7d9fe9224724dc56e3
Kevin Yang <kevinyang.cyen@gmail.com>
2020-10-26 16:14:09 -0700

    shortened DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,26 +3,26 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from 'classnames/bind';
 
-export default function DayListItem(props) {
+export default function DayListItem({spots, selected, name, setDay}) {
   const formatSpots = function() {
-    if (!props.spots) {
+    if (!spots) {
       return "no spots remaining";
     }
-    else if (props.spots === 1) {
+    else if (spots === 1) {
       return "1 spot remaining";
     }
     else {
-      return `${props.spots} spots remaining`
+      return `${spots} spots remaining`
     }
   }
 
   const dayListItemClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": !props.spots
+    "day-list__item--selected": selected,
+    "day-list__item--full": !spots
   })
   return (
-    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
+    <li className={dayListItemClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
