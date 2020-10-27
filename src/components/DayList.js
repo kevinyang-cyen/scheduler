10de6cc5518bf9ca64dd8226cbd85e0954e45579
Kevin Yang <kevinyang.cyen@gmail.com>
2020-10-27 13:28:49 -0700

    added InterviewerListItem js and scss

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import DayListItem from "./DayListItem"
 
-export default function DayList({day, setDay, days, id}) {
+export default function DayList({day, setDay, days}) {
   // days.forEach(day => console.log(day.name));
   // console.log(name);
   return (
     <ul>
-        {days.map(weekDay => <DayListItem 
-        key = {id}
+        {days.map((weekDay) => <DayListItem 
+        key = {weekDay.id}
         name={weekDay.name}
         spots= {weekDay.spots}
         selected= {weekDay.name === day}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+import classNames from 'classnames/bind';
+
+export default function InterviewerListItem({id, name, avatar, selected, setInterviewer}) {
+
+  const interviewerListItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected
+  })
+
+  return (
+    <li className={interviewerListItemClass} onClick={() => setInterviewer(name)}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {name}
+    </li>
+  );
+}
