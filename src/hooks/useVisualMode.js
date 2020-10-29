f82648701bd0804730c4a7ee9445322579852374
Kevin Yang <kevinyang.cyen@gmail.com>
2020-10-29 15:13:20 -0700

    added useVisualMode.js hooks and changed index.js to be able to display empty time slots, then display form when clicked on, also added a back function for onCancel of Form

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace) {
+    if (replace) {
+      history.pop();
+      setMode(newMode);
+      setHistory([...history, newMode]);
+    }
+    else {
+      setMode(newMode);
+      setHistory([...history, newMode]);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length-1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
